ada334992bc65a5ef8845d2dadb02c54d26e3f7b
Beatriz Gonzalez <beatgonza@gmail.com>
2013-07-22 16:32:01 -0600

    Set current_id from account

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,13 @@ class Account < ActiveRecord::Base
   attr_accessible :name, :subdomain
 
   has_and_belongs_to_many :users
+
+  def self.current_id=(id)
+    Thread.current[:account_id] = id
+  end
+
+  def self.current_id
+    Thread.current[:account_id]
+  end
+
 end
