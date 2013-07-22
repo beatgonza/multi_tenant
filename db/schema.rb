c03b58fd7fe88eb5f68dd9c480ecd9b3ee54832f
Beatriz Gonzalez <beatgonza@gmail.com>
2013-07-22 11:41:22 -0600

    Create account

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130722153019) do
+ActiveRecord::Schema.define(:version => 20130722160532) do
+
+  create_table "accounts", :force => true do |t|
+    t.string   "name"
+    t.string   "subdomain"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
