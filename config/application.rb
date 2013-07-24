32a7aa770e2e39918312ae7bfa63fc39531e374d
Beatriz Gonzalez <beatgonza@gmail>
2013-07-23 22:09:55 -0600

    Changes for deploy

--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,5 @@ module MultiTenant
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
   end
 end
