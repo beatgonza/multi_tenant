f61ce92b2324fc214d7c10a3c01c9602c72744bc
Beatriz Gonzalez <beatgonza@gmail.com>
2013-07-25 08:52:40 -0600

    Fix home index test

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,11 +5,11 @@ describe HomeController do
   context "GET index" do
 
     it "is successfull" do
-      pending
       account = FactoryGirl.create(:account)
       Account.current_id = account.id
       user1 = FactoryGirl.create(:user)
       user1.accounts << account
+      sign_in user1
       get :index
       assigns(:users).should == [user1]
     end
