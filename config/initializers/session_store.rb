7b8989b51acbd1fabede78512d04c20726b6eb5f
Beatriz Gonzalez <beatgonza@gmail.com>
2013-07-29 14:21:03 -0600

    Change session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-MultiTenant::Application.config.session_store :cookie_store, key: '_multi_tenant_session', domain: ".multi_tenant.dev"
+MultiTenant::Application.config.session_store :cookie_store, key: '_multi_tenant_session', domain: :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
